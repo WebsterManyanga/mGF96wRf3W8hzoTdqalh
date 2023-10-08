b7f46b190b65e6c23b4e0e20429778e16074578f
WebsterManyanga <109490186+WebsterManyanga@users.noreply.github.com>
2023-10-08 14:02:03 +0000

    added background with animation

--- memory-game/src/App.tsx
+++ memory-game/src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Clouds from './Components/Clouds/Clouds';
 
 function App() {
   return (
     <div className="App">
-      
+      <Clouds />
     </div>
   );
 }

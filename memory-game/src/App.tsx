0d696ecaae0b01ffe3d5ad6fcde39de5715d7438
WebsterManyanga <109490186+WebsterManyanga@users.noreply.github.com>
2023-10-09 10:15:55 +0000

    add responsive menu bar. Buttons not yet active

--- memory-game/src/App.tsx
+++ memory-game/src/App.tsx
@@ -3,12 +3,14 @@ import logo from './logo.svg';
 import './App.css';
 import Clouds from './Components/Clouds/Clouds';
 import Logo from './Components/Logo/Logo';
+import Menu from './Components/Menu/Menu';
 
 function App() {
   return (
     <div className="App">
       <Clouds />
       <Logo />
+      <Menu />
     </div>
   );
 }
---
+++ memory-game/src/Components/Menu/Menu.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Menu.css';
+
+export default function Menu() {
+  return (
+    <main>
+        <div className="menu">
+            <button type="button">Easy</button>
+            <button type="button">MEDIUM</button>
+            <button type="button">HARD</button>
+        </div>
+    </main>
+  )
+}

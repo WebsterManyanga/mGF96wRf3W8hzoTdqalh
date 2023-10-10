81313d895cdde2e9b6d729613bc668d7914094e3
WebsterManyanga <109490186+WebsterManyanga@users.noreply.github.com>
2023-10-10 13:14:15 +0000

    created functional countdown timer with react countdown

--- memory-game/src/Components/Timer/Timer.tsx
+++ memory-game/src/Components/Timer/Timer.tsx
@@ -1,10 +1,33 @@
 import React from 'react';
 import './Timer.css';
+import Countdown from 'react-countdown';
 
 export default function Timer() {
   return (
     <div className='timer'>
-      <div className="counter">58</div>
+      <div className="counter">
+        <Countdown date={Date.now() + 60000} renderer={renderer}/>
+      </div>
     </div>
   )
+}
+
+
+const renderer = ({hours, minutes, seconds, completed}:RendererType) => {
+  if (minutes === 1) {
+    return <span>60</span>
+  }
+  if (completed) {
+    return 'Done';
+  }
+  else {
+    return <span>{seconds}</span>
+  }
+} 
+
+interface RendererType {
+  hours: number,
+  minutes: number,
+  seconds: number,
+  completed: boolean
 }

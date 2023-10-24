c7a8bd1b510d17d27136ec55036d13f06b13b5d4
WebsterManyanga <109490186+WebsterManyanga@users.noreply.github.com>
2023-10-24 09:52:59 +0000

    removed onClick event when card is flipped open

--- memory-game/src/Components/Card/Card.tsx
+++ memory-game/src/Components/Card/Card.tsx
@@ -9,27 +9,62 @@ export default function Card({id, position, cardsList, incrementCount, count, re
     const resetRef = useRef(false);
 
     const flip = () => {
-        flipCard === 'flip_1' ? setFlipCard('flip_2') : setFlipCard('flip_1');
-        if (flipCard === 'flip_2') {
-            incrementCount();
-            if (cardFlippedPosition === -1) {
-                SetCardFlippedPosition(position);
-            } else {
-                if (cardsList[cardFlippedPosition].id === id) {
-                    console.log('score');
+            flipCard === 'flip_1' ? setFlipCard('flip_2') : setFlipCard('flip_1');
+            if (flipCard === 'flip_2') {
+                incrementCount();
+                if (cardFlippedPosition === -1) {
+                    SetCardFlippedPosition(position);
                 } else {
-                    console.log('fail');
-                    setTimeout(
-                        () => {
-                            setFlipCard('flip_2');
-                        }
-                        , 1000);
+                    if (cardsList[cardFlippedPosition].id === id) {
+                        console.log('score');
+                    } else {
+                        console.log('fail');
+                        setTimeout(
+                            () => {
+                                setFlipCard('flip_2');
+                            }
+                            , 1000);
+                    }
                 }
+            } else {
+                resetCount();
+                SetCardFlippedPosition(-1);
             }
-        }
+
 
     }
+    const result = () => {
+        if (flipCard === 'flip_1') {
+            console.log(`count: ${count}`);
 
+            return (
+                <div className='card'>
+                <div className={`card__inner ${flipCard}`}>
+                    <div className="card__back">
+                        <div></div>
+                    </div>
+                    <div className="card__front">
+                        <h1>{id}</h1>
+                    </div>
+                </div>
+            </div>        
+            )
+        }
+        
+        return (
+            <div className='card' onClick={() => flip()}>
+            <div className={`card__inner ${flipCard}`}>
+                <div className="card__back">
+                    <div></div>
+                </div>
+                <div className="card__front">
+                    <h1>{id}</h1>
+                </div>
+            </div>
+            </div>
+    
+        )
+    }
 
     if (resetRef.current) {
 
@@ -40,27 +75,14 @@ export default function Card({id, position, cardsList, incrementCount, count, re
     }
 
     if (cardFlippedPosition === position && count === 2 && !resetRef.current) {
+        console.log(`Count: ${count}`);    
         resetRef.current = true;
         console.log(`closing: ${position}`);
         resetCount();
     }
 
-    console.log(`count: ${count}`);
-    console.log(`cardFlippedPositon: ${cardFlippedPosition}`);
-
-
-  return (
-    <div className='card' onClick={() => flip()}>
-        <div className={`card__inner ${flipCard}`}>
-            <div className="card__back">
-                <div></div>
-            </div>
-            <div className="card__front">
-                <h1>{id}</h1>
-            </div>
-        </div>
-    </div>
-  )
+    const output = result();
+    return output;
 }
 
 interface Proptype {
